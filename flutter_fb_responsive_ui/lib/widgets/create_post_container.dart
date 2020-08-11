e19985dd544f4a5090c303003c20172566e20072
Mohsin <follow.mohsin@gmail.com>
2020-08-11 20:31:57 +0600

    fb ui web done

--- flutter_fb_responsive_ui/lib/widgets/create_post_container.dart
+++ flutter_fb_responsive_ui/lib/widgets/create_post_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/responsive.dart';
 
 class CreatePostContainer extends StatelessWidget {
   final User currentUser;
@@ -11,69 +12,79 @@ class CreatePostContainer extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                ProfileAvatar(
-                  imageUrl: currentUser.imageUrl,
-                  isActive: false,
-                ),
-                const SizedBox(
-                  width: 8.0,
-                ),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'What\'s on your mind?',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Divider(
-              height: 10.0,
-              thickness: 0.5,
-            ),
-            Container(
-              height: 40.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            )
+          : null,
+      child: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.videocam,
-                      color: Colors.red,
-                    ),
-                    label: Text('Live'),
+                  ProfileAvatar(
+                    imageUrl: currentUser.imageUrl,
+                    isActive: false,
                   ),
-                  const VerticalDivider(width: 8.0),
-                  FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.photo_library,
-                      color: Colors.green,
-                    ),
-                    label: Text('Photo'),
+                  const SizedBox(
+                    width: 8.0,
                   ),
-                  const VerticalDivider(width: 8.0),
-                  FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.video_call,
-                      color: Colors.purpleAccent,
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration.collapsed(
+                        hintText: 'What\'s on your mind?',
+                      ),
                     ),
-                    label: Text('Room'),
                   ),
                 ],
               ),
-            ),
-          ],
+              const Divider(
+                height: 10.0,
+                thickness: 0.5,
+              ),
+              Container(
+                height: 40.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.videocam,
+                        color: Colors.red,
+                      ),
+                      label: Text('Live'),
+                    ),
+                    const VerticalDivider(width: 8.0),
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.photo_library,
+                        color: Colors.green,
+                      ),
+                      label: Text('Photo'),
+                    ),
+                    const VerticalDivider(width: 8.0),
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.video_call,
+                        color: Colors.purpleAccent,
+                      ),
+                      label: Text('Room'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- flutter_fb_responsive_ui/lib/widgets/stories.dart
+++ flutter_fb_responsive_ui/lib/widgets/stories.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/responsive.dart';
 
 class Stories extends StatelessWidget {
   final User currentUser;
@@ -18,7 +19,7 @@ class Stories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 200,
-      color: Colors.white,
+      color: Responsive.isDesktop(context) ? Colors.transparent : Colors.white,
       child: ListView.builder(
         padding: EdgeInsets.symmetric(
           horizontal: 8.0,
@@ -78,6 +79,15 @@ class _StoryCard extends StatelessWidget {
           decoration: BoxDecoration(
             gradient: Palette.storyGradient,
             borderRadius: BorderRadius.circular(12.0),
+            boxShadow: Responsive.isDesktop(context)
+                ? const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(0, 2),
+                      blurRadius: 4.0,
+                    ),
+                  ]
+                : null,
           ),
         ),
         Positioned(
